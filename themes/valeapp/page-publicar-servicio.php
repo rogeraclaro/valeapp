e24782c99fd297d1c5813bc84d3c43756b78b439
nick1708 <nick170899@gmail.com>
2023-12-07 15:38:28 -0600

    Publish Sservice page, Done

--- themes/valeapp/page-publicar-servicio.php
+++ themes/valeapp/page-publicar-servicio.php
@@ -22,22 +22,26 @@ if (current_user_can('editor') || current_user_can('administrator') || current_u
     require_once plugin_dir_path(__FILE__) . 'inc/membership/post-limits.php';
 
 ?>
-<div class="container mt-5">
-    <div class="row justify-content-center align-items-center">
-        <div class="col-12">
-            <?php
-                acf_form([
-                    'post_id'       => 'new_post',
-                    'field_groups'  => ['group_64e3cd0b38f67'],
-                    'new_post'      => [
-                        'post_type'     => 'publicar-servicio',
-                        'post_status'   => 'publish'
-                    ],
-                    'submit_value'  => 'Enviar',
-                    'updated_message' => '¡Formulario actualizado!',
-                    'return' => '/home',
-                ]);
-            ?>
+<div class="PublishService">
+    <div class="container mt-5">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-12">
+            <h2 class="title">Catálogo de servicios</h2>
+                <?php
+                    acf_form([
+                        'post_id'       => 'new_post',
+                        'field_groups'  => ['group_64e3cd0b38f67'],
+                        'new_post'      => [
+                            'post_type'     => 'publicar-servicio',
+                            'post_status'   => 'publish'
+                        ],
+                        'uploader' => 'wp',
+                        'submit_value'  => 'Confirmar',
+                        'updated_message' => '¡Formulario actualizado!',
+                        'return' => '/home',
+                    ]);
+                ?>
+            </div>
         </div>
     </div>
 </div>
