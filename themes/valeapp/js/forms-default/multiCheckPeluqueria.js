e7bd4ed69f6aabf5fbbc0ffdb80819ea8375e476
nick1708 <nick170899@gmail.com>
2024-01-31 08:45:59 -0600

    fixed change funtion peluqueria

--- themes/valeapp/js/forms-default/multiCheckPeluqueria.js
+++ themes/valeapp/js/forms-default/multiCheckPeluqueria.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var selectedOptions = [];
 
+    // Habilitar o deshabilitar el botón de la primera modal según las selecciones
     $('.peluqueria-item input').on('change', function () {
         selectedOptions = $('.peluqueria-item input:checked').not('[data-tipo="todos"]').map(function () {
             return $(this).data('tipo');
@@ -10,19 +11,23 @@ $(document).ready(function () {
         $('#btnSubmit').prop('disabled', selectedCount === 0);
     });
 
+    // Marcar todas las opciones al seleccionar "Todos"
     $('[data-peluqueria-selectAll="true"]').on('change', function () {
         var isChecked = $(this).prop('checked');
         $('.peluqueria-item input').prop('checked', isChecked).trigger('change');
     });
 
+    // Capturar las opciones seleccionadas en la primera modal
     $('#btnSubmit').on('click', function () {
         console.log('Opciones seleccionadas en la primera modal:', selectedOptions);
     });
 
+    // Habilitar o deshabilitar el botón de la segunda modal según las selecciones
     $('.gender-item input').on('change', function () {
         $('#btnSubmitGender').prop('disabled', !$('.gender-item input:checked').length);
     });
 
+    // Capturar el género seleccionado en la segunda modal
     $('#btnSubmitGender').on('click', function () {
         var selectedGender = $('.gender-item input:checked').data('tipo');
         console.log('Género seleccionado en la segunda modal:', selectedGender);
