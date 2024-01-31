254e02fba5c1ec84b5136158015268f3b73acee7
nick1708 <nick170899@gmail.com>
2024-01-31 11:05:55 -0600

    Changed the syntax of the multicheckPeluqueria function

--- themes/valeapp/js/forms-default/multiCheckPeluqueria.js
+++ themes/valeapp/js/forms-default/multiCheckPeluqueria.js
@@ -1,35 +1,45 @@
-$(document).ready(function () {
-    var selectedOptions = [];
-
-    // Habilitar o deshabilitar el botón de la primera modal según las selecciones
-    $('.peluqueria-item input').on('change', function () {
+jQuery(document).ready(function ($) {
+    function updatePeluqueriaOptions() {
         selectedOptions = $('.peluqueria-item input:checked').not('[data-tipo="todos"]').map(function () {
             return $(this).data('tipo');
         }).get();
-        var selectedCount = selectedOptions.length;
+        let selectedCount = selectedOptions.length;
         $('#contador').text('Opciones seleccionadas: ' + selectedCount);
         $('#btnSubmit').prop('disabled', selectedCount === 0);
-    });
+    }
+
+    function selectAllPeluqueria() {
+        let isChecked = $(this).prop('checked');
+        $('.peluqueria-item input').prop('checked', isChecked).trigger('change');
+    }
+
+    function capturePeluqueriaOptions() {
+        console.log('Opciones seleccionadas en la primera modal:', selectedOptions);
+    }
+
+    function updateGenderOptions() {
+        $('#btnSubmitGender').prop('disabled', !$('.gender-item input:checked').length);
+    }
+
+    function captureSelectedGender() {
+        let selectedGender = $('.gender-item input:checked').data('tipo');
+        console.log('Género seleccionado en la segunda modal:', selectedGender);
+    }
+
+    let selectedOptions = [];
+
+    // Habilitar o deshabilitar el botón de la primera modal según las selecciones
+    $('.peluqueria-item input').on('change', updatePeluqueriaOptions);
 
     // Marcar todas las opciones al seleccionar "Todos"
-    $('[data-peluqueria-selectAll="true"]').on('change', function () {
-        var isChecked = $(this).prop('checked');
-        $('.peluqueria-item input').prop('checked', isChecked).trigger('change');
-    });
+    $('[data-peluqueria-selectAll="true"]').on('change', selectAllPeluqueria);
 
     // Capturar las opciones seleccionadas en la primera modal
-    $('#btnSubmit').on('click', function () {
-        console.log('Opciones seleccionadas en la primera modal:', selectedOptions);
-    });
+    $('#btnSubmit').on('click', capturePeluqueriaOptions);
 
     // Habilitar o deshabilitar el botón de la segunda modal según las selecciones
-    $('.gender-item input').on('change', function () {
-        $('#btnSubmitGender').prop('disabled', !$('.gender-item input:checked').length);
-    });
+    $('.gender-item input').on('change', updateGenderOptions);
 
     // Capturar el género seleccionado en la segunda modal
-    $('#btnSubmitGender').on('click', function () {
-        var selectedGender = $('.gender-item input:checked').data('tipo');
-        console.log('Género seleccionado en la segunda modal:', selectedGender);
-    });
+    $('#btnSubmitGender').on('click', captureSelectedGender);
 });
