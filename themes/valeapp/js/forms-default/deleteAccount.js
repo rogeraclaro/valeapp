deee1baac20fcf5c13a1cd4278b8c1283f21df30
nick1708 <nick170899@gmail.com>
2024-02-08 14:35:00 -0600

    Modified the delete account function

--- themes/valeapp/js/forms-default/deleteAccount.js
+++ themes/valeapp/js/forms-default/deleteAccount.js
@@ -1,35 +1,22 @@
-    // Variables globales
-    let selectedIds = [];
+let selectedIds = [];
 
-    // Función que se ejecuta cuando cambia el estado de un checkbox
-    $('.EliminarCuenta-input').change(function() {
-      // Actualiza el contador y captura los IDs seleccionados
-      updateSelection();
-    });
+jQuery('.EliminarCuenta-input').change(function () {
+    updateSelection();
+});
 
-    // Función para actualizar el contador y los IDs seleccionados
-    function updateSelection() {
-      let selectedCount = 0;
-      selectedIds = [];
+function updateSelection() {
+    let selectedCount = 0;
+    selectedIds = [];
 
-      // Recorre todos los checkboxes
-      $('.EliminarCuenta-input').each(function() {
-        // Verifica si el checkbox está seleccionado
-        if ($(this).prop('checked')) {
-          // Incrementa el contador y agrega el ID a la lista
-          selectedCount++;
-          selectedIds.push($(this).attr('id'));
+    jQuery('.EliminarCuenta-input').each(function () {
+        if (jQuery(this).prop('checked')) {
+            selectedCount++;
+            selectedIds.push(jQuery(this).attr('id'));
         }
-      });
-
-      // Actualiza el contador
-      $('#selectedCount').text(selectedCount);
-    }
-
-    // Función que se ejecuta al hacer clic en el botón "Confirmar"
-    $('#ConfirmDelete').click(function() {
-      // Muestra los IDs seleccionados en la consola
-      console.log('IDs seleccionados:', selectedIds);
-
-      // También podrías realizar otras acciones aquí con los IDs seleccionados
     });
+    jQuery('#selectedCount').text(selectedCount);
+}
+
+jQuery('#ConfirmDelete').click(function () {
+    console.log('IDs seleccionados:', selectedIds);
+});
