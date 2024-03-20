8e6ec4ce1da4b6e47644dd9709b929ecb2285dea
Shoji <shojiurid@gmail.com>
2024-03-20 07:47:32 -0600

    Added desktop footer

--- themes/valeapp/footer.php
+++ themes/valeapp/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -13,40 +14,103 @@
 <footer>
 	<div class="container">
 		<div id="accordionfooter" class="accordion accordion-buttons">
+			<div class="d-none d-md-block footer-logo">
+				<img class="image-fluid image footer-logo-img" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-footer-logo.png" alt="ValeApp">
+				<div class="footer-logo-text">
+					La tasca que necessitis, de la manera<br />
+					més fácile. Vale connecta professionals<br />
+					que ofereizen serveis en diverses<br />
+					árees, amb clients que necessiten<br />
+					ajuda amb tasques quotidianes i<br />
+					projectes especialitzats, a prop teu.
+				</div>
+			</div>
 			<div class="accordion-item">
 				<h2 id="headingPoints" class="accordion-header">
-					<button type="button" class="accordion-button event collapsed" data-label="collapsefaq"
-						data-bs-toggle="collapse" data-bs-target="#collapsefaq" aria-expanded="false"
-						aria-controls="collapsefaq">Categorías <img class="img-fluid"
-							src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-providers-chevron-faq.png"
-							alt="ValeApp"></button>
+					<button type="button" class="accordion-button event collapsed" data-label="collapsefaq" data-bs-toggle="collapse" data-bs-target="#collapsefaq" aria-expanded="false" aria-controls="collapsefaq">Categorías <img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-providers-chevron-faq.png" alt="ValeApp"></button>
 				</h2>
-				<div id="collapsefaq" class="accordion-collapse collapse" aria-labelledby="headingPoints"
-					data-bs-parent="#accordionfqa">
+				<div id="collapsefaq" class="accordion-collapse collapse" aria-labelledby="headingPoints" data-bs-parent="#accordionfqa">
 					<div class="accordion-body">
-						<p>
+						<!-- <p>
 							Lorem ipsum dolor sit amet, consectetur <br class="xs-br">
 							adipisicing elit, sed do eiusmod tempor <br class="xs-br">
 							incididunt ut labore et dolore magna aliqua. <br class="xs-br">
 							Ut enim ad minim veniam, quis nostrud <br class="xs-br">
 							exercitation ullamco laboris.
-						</p>
+						</p> -->
+						<ul class="accordion-links row">
+							<li class="col-12 col-md-5">
+								<a href="#">llar</a>
+							</li>
+							<li class="col-12 col-md-7">
+								<a href="#">Bellesa</a>
+							</li>
+							<li class="col-12 col-md-5">
+								<a href="#">Aprén</a>
+							</li>
+							<li class="col-12 col-md-7">
+								<a href="#">Contracta</a>
+							</li>
+							<li class="col-12 col-md-5">
+								<a href="#">Esport</a>
+							</li>
+							<li class="col-12 col-md-7">
+								<a href="#">Lloga</a>
+							</li>
+							<li class="col-12 col-md-5">
+								<a href="#">Ment</a>
+							</li>
+							<li class="col-12 col-md-7">
+								<a href="#">Esdeveniments</a>
+							</li>
+							<li class="col-12">
+								<a href="#">Cuida els teus</a>
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
 			<div class="accordion-item">
 				<h2 id="headingPoints" class="accordion-header">
-					<button type="button" class="accordion-button event collapsed" data-label="collapsefaq2"
-						data-bs-toggle="collapse" data-bs-target="#collapsefaq2" aria-expanded="false"
-						aria-controls="collapsefaq2">
+					<button type="button" class="accordion-button event collapsed" data-label="collapsefaq2" data-bs-toggle="collapse" data-bs-target="#collapsefaq2" aria-expanded="false" aria-controls="collapsefaq2">
 						Vale
-						<img class="img-fluid"
-							src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-providers-chevron-faq.png"
-							alt="ValeApp">
+						<img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-providers-chevron-faq.png" alt="ValeApp">
 					</button>
 				</h2>
-				<div id="collapsefaq2" class="accordion-collapse collapse" aria-labelledby="headingPoints"
-					data-bs-parent="#accordionfqa">
+				<div id="collapsefaq2" class="accordion-collapse collapse" aria-labelledby="headingPoints" data-bs-parent="#accordionfqa">
+					<div class="accordion-body">
+						<!-- <p>
+							Lorem ipsum dolor sit amet, consectetur <br class="xs-br">
+							adipisicing elit, sed do eiusmod tempor <br class="xs-br">
+							incididunt ut labore et dolore magna aliqua. <br class="xs-br">
+							Ut enim ad minim veniam, quis nostrud <br class="xs-br">
+							exercitation ullamco laboris.
+						</p> -->
+						<ul class="accordion-links row">
+							<li class="col-12">
+								<a href="#">Preguntes freqúents</a>
+							</li>
+							<li class="col-12">
+								<a href="#">Professionals</a>
+							</li>
+							<li class="col-12">
+								<a href="#">Registrar-se/Iniciar sessió</a>
+							</li>
+							<li class="col-12">
+								<a href="#">Blog</a>
+							</li>
+							<li class="col-12">
+								<a href="#">Ajuda</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="accordion-item d-block d-md-none">
+				<h2 id="headingPoints" class="accordion-header">
+					<button type="button" class="accordion-button event collapsed" data-label="collapsefaq3" data-bs-toggle="collapse" data-bs-target="#collapsefaq3" aria-expanded="false" aria-controls="collapsefaq3">Acerca de <img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-providers-chevron-faq.png" alt="ValeApp"></button>
+				</h2>
+				<div id="collapsefaq3" class="accordion-collapse collapse" aria-labelledby="headingPoints" data-bs-parent="#accordionfqa">
 					<div class="accordion-body">
 						<p>
 							Lorem ipsum dolor sit amet, consectetur <br class="xs-br">
@@ -60,52 +124,42 @@
 			</div>
 			<div class="accordion-item">
 				<h2 id="headingPoints" class="accordion-header">
-					<button type="button" class="accordion-button event collapsed" data-label="collapsefaq3"
-						data-bs-toggle="collapse" data-bs-target="#collapsefaq3" aria-expanded="false"
-						aria-controls="collapsefaq3">Acerca de <img class="img-fluid"
-							src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-providers-chevron-faq.png"
-							alt="ValeApp"></button>
+					<button type="button" class="accordion-button event collapsed" data-label="collapsefaq4" data-bs-toggle="collapse" data-bs-target="#collapsefaq4" aria-expanded="false" aria-controls="collapsefaq4">Legal <img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-providers-chevron-faq.png" alt="ValeApp"></button>
 				</h2>
-				<div id="collapsefaq3" class="accordion-collapse collapse" aria-labelledby="headingPoints"
-					data-bs-parent="#accordionfqa">
+				<div id="collapsefaq4" class="accordion-collapse collapse" aria-labelledby="headingPoints" data-bs-parent="#accordionfqa">
 					<div class="accordion-body">
-						<p>
+						<!-- <p>
 							Lorem ipsum dolor sit amet, consectetur <br class="xs-br">
 							adipisicing elit, sed do eiusmod tempor <br class="xs-br">
 							incididunt ut labore et dolore magna aliqua. <br class="xs-br">
 							Ut enim ad minim veniam, quis nostrud <br class="xs-br">
 							exercitation ullamco laboris.
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="accordion-item">
-				<h2 id="headingPoints" class="accordion-header">
-					<button type="button" class="accordion-button event collapsed" data-label="collapsefaq4"
-						data-bs-toggle="collapse" data-bs-target="#collapsefaq4" aria-expanded="false"
-						aria-controls="collapsefaq4">Legal <img class="img-fluid"
-							src="<?php echo get_stylesheet_directory_uri(); ?>/img/valeapp-providers-chevron-faq.png"
-							alt="ValeApp"></button>
-				</h2>
-				<div id="collapsefaq4" class="accordion-collapse collapse" aria-labelledby="headingPoints"
-					data-bs-parent="#accordionfqa">
-					<div class="accordion-body">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur <br class="xs-br">
-							adipisicing elit, sed do eiusmod tempor <br class="xs-br">
-							incididunt ut labore et dolore magna aliqua. <br class="xs-br">
-							Ut enim ad minim veniam, quis nostrud <br class="xs-br">
-							exercitation ullamco laboris.
-						</p>
+						</p> -->
+						<ul class="accordion-links row">
+							<li class="col-12">
+								<a href="#">Termes i condicions</a>
+							</li>
+							<li class="col-12">
+								<a href="#">Política de privacitat</a>
+							</li>
+							<li class="col-12">
+								<a href="#">Política de cookies</a>
+							</li>
+							<li class="col-12">
+								<a href="#">Política de cancel-lació</a>
+							</li>
+							<li class="col-12">
+								<a href="#">Avís Legal</a>
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
 		</div>
 		<div class="footlast">
-			<img class="image-fluid image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valelogo.png"
-				alt="ValeApp">
+			<img class="image-fluid image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/valelogo.png" alt="ValeApp">
 			<div class="copyright">
-				Copyright © VALE 2023
+				© 2024 Vale. Tots els drets reservats.
 			</div>
 			<div class="xxss">
 				<a href="#" target="_blank"><i class="bi bi-facebook"></i></a>
